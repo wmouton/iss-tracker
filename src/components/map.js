8de4707f0eb13fcb0d58e5d06443dec721c2f453
l33th <l33th@protonmail.ch>
2022-01-12 08:26:11 +0200

    api key setup

--- src/components/map.js
+++ src/components/map.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-const Map = () => {
-    return (
-        <div className='map-container'>
-            <GoogleMapReact bootstrapURLKeys={{key: API_KEY}}>
+const Map = ({ center, zoom }) => {
+  return (
+    <div className='map-container'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-            </GoogleMapReact>
-        </div>
-    )
-}
+Map.defaultProps = {
+  center: {
+    lat: 51.5074,
+    lng: 0.1278,
+  },
+  zoom: 6,
+};
 
-export default Map
+export default Map;
