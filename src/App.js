e22b0e57a8b488856f7ca968bf1b0251429de1bb
l33th <l33th@protonmail.ch>
2022-01-12 07:46:58 +0200

    map setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Map from './components/map';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>International Space Station Tracker</h1>
-      </header>
+    <div className='App'>
+      <Map/>
     </div>
   );
 }
