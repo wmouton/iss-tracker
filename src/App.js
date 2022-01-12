cc944561980988cbb572cd6278b2dd117a874115
l33th <l33th@protonmail.ch>
2022-01-12 09:14:17 +0200

    axios data not returning

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Map from './components/map';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const url = 'http://api.open-notify.org/iss-now.json';
+
 function App() {
   const [loading, SetLoading] = useState(false);
-  const [longetude, setLongetude] = useState(0.1278);
+  const [longitude, setLongitude] = useState(0.1278);
   const [latitude, setLatitude] = useState(51.5074);
 
   useEffect(() => {
@@ -14,12 +16,18 @@ function App() {
 
   const getLocation = async () => {
     SetLoading(true);
+    const res = await axios.get(url);
+    const { longitude, latitude } = await res.date.iss_position;
+
+    setLongitude(parseFloat(longitude));
+    setLatitude(parseFloat(latitude));
+    SetLoading(false);
   };
 
   return (
     <div className='App'>
       {!loading ? (
-        <Map center={{ lat: latitude, lng: longetude }} />
+        <Map center={{ lat: latitude, lng: longitude }} zoom={6} />
       ) : (
         <h1>Loading</h1>
       )}
