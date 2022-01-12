7053f09329236aa8d634e5b46a3eae32e42fbae8
l33th <l33th@protonmail.ch>
2022-01-12 08:43:36 +0200

    getLocation setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Map from './components/map';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, SetLoading] = useState(false);
   const [longetude, setLongetude] = useState(0.1278);
   const [latitude, setLatitude] = useState(51.5074);
+
+  const getLocation = async () => {
+    SetLoading(true);
+  };
 
   return (
     <div className='App'>
