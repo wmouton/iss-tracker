5ce2b71b3c3491c57c86fead1ae1b41627efcd7e
l33th <l33th@protonmail.ch>
2022-01-12 08:41:43 +0200

    latitude and longetude states

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [longetude, setLongetude] = useState(0.1278);
+  const [latitude, setLatitude] = useState(51.5074);
 
   return (
     <div className='App'>
-      {!loading ? <Map /> : <h1>Loading</h1>}
+      {!loading ? (
+        <Map center={{ lat: latitude, lng: longetude }} />
+      ) : (
+        <h1>Loading</h1>
+      )}
     </div>
   );
 }
